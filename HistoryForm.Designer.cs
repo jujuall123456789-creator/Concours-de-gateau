007f933076e8088845b1c092aba7c11e591eb4c1
jujuall123456789-creator <juju.all123456789@gmail.com>
2026-04-25 06:39:55 +0200

    Add files via upload

---
+++ HistoryForm.Designer.cs
@@ -0,0 +1,39 @@
+[
+  {
+    "Date": "28/04/2026",
+    "Theme": "\uD83C\uDF4B G\u00E2teau moelleux au citron \uD83C\uDF4B",
+    "ChallengersList": [
+      "Maxime Y \uD83D\uDC16",
+      "Nicolas \uD83E\uDD84"
+    ],
+    "Challengers": "Maxime Y \uD83D\uDC16 vs Nicolas \uD83E\uDD84"
+  },
+  {
+    "Date": "28/04/2026",
+    "Theme": "\uD83C\uDF4B G\u00E2teau moelleux au citron \uD83C\uDF4B",
+    "ChallengersList": [
+      "Tianning \uD83E\uDDAB",
+      "Maxime R \uD83E\uDD98"
+    ],
+    "Challengers": "Tianning \uD83E\uDDAB vs Maxime R \uD83E\uDD98"
+  },
+  {
+    "Date": "23/04/2026",
+    "Theme": "\uD83C\uDF4B G\u00E2teau moelleux au citron \uD83C\uDF4B",
+    "ChallengersList": [
+      "Maxime R \uD83E\uDD98",
+      "Beno\u00EEt \uD83D\uDC17"
+    ],
+    "Challengers": "Maxime R \uD83E\uDD98 vs Beno\u00EEt \uD83D\uDC17"
+  },
+  {
+    "Date": "23/04/2026",
+    "Theme": "\uD83C\uDF4B G\u00E2teau moelleux au citron \uD83C\uDF4B",
+    "ChallengersList": [
+      "Maxime Y \uD83D\uDC16",
+      "Tianning \uD83E\uDDAB",
+      "Mathilde \uD83E\uDD89"
+    ],
+    "Challengers": "Maxime Y \uD83D\uDC16 vs Tianning \uD83E\uDDAB vs Mathilde \uD83E\uDD89"
+  }
+]
---
+++ TemplateService.cs
@@ -0,0 +1,65 @@
+﻿using DuelDeGateaux.Models;
+using DuelDeGateaux.Tools;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DuelDeGateaux.Services
+{
+    internal static class ConfigService
+    {
+        // 📁 Chemin du fichier JSON (dans dossier exe)
+        private const string FileName = "appsettings.json";
+
+        /// <summary>
+        /// Chemin du fichier de configuration JSON
+        /// </summary>
+#if DEBUG
+        private static readonly string path = Path.Combine("../../../", FileName);
+#else
+        private static readonly string path=Path.Combine(AppDomain.CurrentDomain.BaseDirectory,FileName);
+#endif
+        public static AppConfig Load()
+        {
+            /// <summary>
+            /// Charge la configuration depuis le fichier JSON
+            /// </summary>
+            if (!FileHelper.FileExist(path))
+            {
+                //Si le fichier est absent retour d'un message d'erreur
+                MessageBox.Show($"Config introuvable : {path}");
+                Environment.Exit(1);
+            }
+
+            var json = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<AppConfig>(json) ?? new AppConfig();
+        }
+        /// <summary>
+        /// Sauvegarde la configuration faite par l'utilisateur dans le fichier appsetting.json
+        /// </summary>
+        /// <param name="config"></param>
+        public static void Save(AppConfig config)
+        {
+            File.WriteAllText(path, JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true, }));
+        }
+
+        /// <summary>
+        /// Ouvre le fichier de configuration JSON dans l'éditeur notepad.exe
+        /// </summary>
+        public static void OpenConfigFile()
+        {
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("Fichier config introuvable");
+                return;
+            }
+
+            Process.Start("notepad.exe", path);
+        }
+
+    }
+}
