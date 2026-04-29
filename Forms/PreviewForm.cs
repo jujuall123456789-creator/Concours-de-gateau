da3cf42c90dac8bcb65876e3040a447fbd650bbc
jujuall123456789-creator <juju.all123456789@gmail.com>
2026-04-29 11:43:24 +0200

    Ajotu preview Form

---
+++ Forms/PreviewForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DuelDeGateaux.Forms
+{
+    public partial class PreviewForm : Form
+    {
+        public PreviewForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
