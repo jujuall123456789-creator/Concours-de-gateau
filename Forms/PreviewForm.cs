f414d7d298289d8aee4f668132acde6bf11364ee
jujuall123456789-creator <juju.all123456789@gmail.com>
2026-04-29 11:44:38 +0200

    Implement WebView2 in PreviewForm for HTML preview
    Added a WebView2 component to display HTML content in the PreviewForm.

--- Forms/PreviewForm.cs
+++ Forms/PreviewForm.cs
@@ -1,20 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
+using Microsoft.Web.WebView2.WinForms;
 
 namespace DuelDeGateaux.Forms
 {
     public partial class PreviewForm : Form
     {
-        public PreviewForm()
+        private WebView2 webView;
+        private string htmlContent;
+
+        public PreviewForm(string html, string title)
         {
-            InitializeComponent();
+            htmlContent = html;
+            
+            // Configuration de base de la fenêtre
+            Text = "👁️ Aperçu : " + title;
+            Width = 850;
+            Height = 900;
+            StartPosition = FormStartPosition.CenterParent;
+            ShowIcon = false;
+
+            // Création dynamique du composant WebView2
+            webView = new WebView2
+            {
+                Dock = DockStyle.Fill
+            };
+            Controls.Add(webView);
+
+            // Le composant WebView2 doit être initialisé de manière asynchrone
+            InitializeWebView();
+        }
+
+        private async void InitializeWebView()
+        {
+            // Attend que le moteur Edge soit prêt
+            await webView.EnsureCoreWebView2Async(null);
+            
+            // Injecte notre code HTML directement dans le navigateur intégré
+            webView.NavigateToString(htmlContent);
         }
     }
 }
